1aa00e66ce694406e7bf04418aebfc8c1a3547bf
Achilles-Dev <solohag92@gmail.com>
2023-11-15 15:39:36 +0000

    Add 3 new web3js functions

--- app/(tools)/tools/web3js-online/page.tsx
+++ app/(tools)/tools/web3js-online/page.tsx
@@ -2,69 +2,92 @@ import FunctionContainer from '@/components/FunctionContainer'
 import AsciiToHex from '@/components/web3js/AsciiToHex'
 import BytesToHex from '@/components/web3js/BytesToHex'
 import ConvertScalarValue from '@/components/web3js/ConvertScalarValue'
+import Uint8ArrayEquals from '@/components/web3js/Uint8ArrayEquals'
+import Utf8ToBytes from '@/components/web3js/Utf8ToBytes'
+import Utf8ToHex from '@/components/web3js/Utf8ToHex'
 import {
-    Accordion,
-    AccordionDetails,
-    AccordionSummary,
-    Button,
-    Divider,
-    FormControl,
-    FormHelperText,
-    FormLabel,
-    Input,
-    Sheet,
-    Stack,
-    Textarea,
-    ToggleButtonGroup,
-    Typography,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  Divider,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Sheet,
+  Stack,
+  Textarea,
+  ToggleButtonGroup,
+  Typography,
 } from '@mui/joy'
 
 export default async function Web3JsOnline() {
-    return (
-        <Stack
-            direction={{ xs: 'column', md: 'column' }}
-            sx={{
-                height: '100%',
-                width: '100%',
-            }}
-            spacing={2}
+  return (
+    <Stack
+      direction={{ xs: 'column', md: 'column' }}
+      sx={{
+        height: '100%',
+        width: '100%',
+      }}
+      spacing={2}
+    >
+      <Typography level='h1'>Web3.js Online</Typography>
+      <Typography level='h2'>
+        All available web3-utils functions will be placed below, each inside a
+        FunctionContainer.
+      </Typography>
+
+      <Stack
+        direction={{ xs: 'column', md: 'column' }}
+        sx={{
+          height: '100%',
+          width: '100%',
+        }}
+        overflow={'scroll'}
+        spacing={2}
+      >
+        <FunctionContainer
+          title='web3.uint8ArrayEquals'
+          description='Convert a utf8 string to Bytes.'
         >
-            <Typography level="h1">Web3.js Online</Typography>
-            <Typography level="h2">
-                All available web3-utils functions will be placed below, each inside a
-                FunctionContainer.
-            </Typography>
+          <Uint8ArrayEquals />
+        </FunctionContainer>
 
-            <Stack
-                direction={{ xs: 'column', md: 'column' }}
-                sx={{
-                    height: '100%',
-                    width: '100%',
-                }}
-                overflow={'scroll'}
-                spacing={2}
-            >
-                <FunctionContainer
-                    title="web3.asciiToHex"
-                    description="Get hex representation (prefixed by 0x) of ascii string."
-                >
-                    <AsciiToHex />
-                </FunctionContainer>
+        <FunctionContainer
+          title='web3.utf8ToBytes'
+          description='Convert a utf8 string to Bytes.'
+        >
+          <Utf8ToBytes />
+        </FunctionContainer>
 
-                <FunctionContainer
-                    title="web3.convertScalarValue"
-                    description="Convert a value depending on the format."
-                >
-                    <ConvertScalarValue />
-                </FunctionContainer>
+        <FunctionContainer
+          title='web3.utf8ToHex'
+          description='Get hex representation (prefixed by 0x) of utf8 string.'
+        >
+          <Utf8ToHex />
+        </FunctionContainer>
+        <FunctionContainer
+          title='web3.asciiToHex'
+          description='Get hex representation (prefixed by 0x) of ascii string.'
+        >
+          <AsciiToHex />
+        </FunctionContainer>
 
-                <FunctionContainer
-                    title="web3.bytesToHex"
-                    description="Convert a byte array to a hex string."
-                >
-                    <BytesToHex />
-                </FunctionContainer>
-            </Stack>
-        </Stack>
-    )
+        <FunctionContainer
+          title='web3.convertScalarValue'
+          description='Convert a value depending on the format.'
+        >
+          <ConvertScalarValue />
+        </FunctionContainer>
+
+        <FunctionContainer
+          title='web3.bytesToHex'
+          description='Convert a byte array to a hex string.'
+        >
+          <BytesToHex />
+        </FunctionContainer>
+      </Stack>
+    </Stack>
+  )
 }
---
+++ components/web3js/Uint8ArrayEquals.tsx
@@ -0,0 +1,141 @@
+'use client'
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Sheet,
+  Stack,
+  Typography,
+} from '@mui/joy'
+import { useEffect, useState } from 'react'
+import { uint8ArrayEquals } from 'web3-utils'
+
+export default function Uint8ArrayEquals() {
+  const [byteA, setByteA] = useState<Uint8Array>()
+  const [byteB, setByteB] = useState<Uint8Array>()
+
+  const [output, setOutput] = useState<boolean>()
+
+  const handleByteAChange = (event: React.BaseSyntheticEvent) => {
+    const value = event.target.value
+
+    if (!value || value === '') {
+      setByteA(undefined)
+      return
+    }
+
+    const bytesArray = value
+      .split(',')
+      .map((byte: string) => Number(byte.trim()))
+
+    // finally, convert the array of numbers to a Uint8Array
+    const bytesUint8Array = Uint8Array.from(bytesArray)
+
+    setByteA(bytesUint8Array)
+  }
+
+  const handleByteBChange = (event: React.BaseSyntheticEvent) => {
+    const value = event.target.value
+
+    if (!value || value === '') {
+      setByteB(undefined)
+      return
+    }
+
+    const bytesArray = value
+      .split(',')
+      .map((byte: string) => Number(byte.trim()))
+
+    // finally, convert the array of numbers to a Uint8Array
+    const bytesUint8Array = Uint8Array.from(bytesArray)
+
+    setByteB(bytesUint8Array)
+  }
+
+  useEffect(() => {
+    if (!byteA || !byteB || byteA.length === 0 || byteB.length === 0) {
+      setOutput(undefined)
+      return
+    }
+    console.log(uint8ArrayEquals(byteA, byteB))
+    setOutput(uint8ArrayEquals(byteA, byteB))
+  }, [byteA, byteB])
+
+  return (
+    <Stack
+      direction={{
+        sm: 'column',
+        md: 'row',
+      }}
+      spacing={2}
+      height={'100%'}
+      alignItems={'center'}
+    >
+      <Sheet
+        sx={{
+          height: '100%',
+          width: '100%',
+          maxWidth: {
+            md: '50%',
+            sm: '100%',
+          },
+          alignSelf: 'center',
+        }}
+      >
+        <FormControl size='lg' required={true}>
+          <FormLabel>bytes A Eg. "12, 34, 56, 78"</FormLabel>
+          <Input
+            name='bytes'
+            placeholder={'12, 34, 56, 78'}
+            onChange={handleByteAChange}
+            type='string'
+          />
+        </FormControl>
+        <FormControl size='lg' required={true}>
+          <FormLabel>bytes B Eg. "12, 34, 56, 78"</FormLabel>
+          <Input
+            name='bytes'
+            placeholder={'12, 34, 56, 78'}
+            onChange={handleByteBChange}
+            type='string'
+          />
+        </FormControl>
+      </Sheet>
+
+      <Sheet
+        sx={{
+          height: '100%',
+          minHeight: '100px',
+          width: '100%',
+          maxWidth: {
+            md: '50%',
+            sm: '100%',
+          },
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          alignSelf: 'center',
+          borderRadius: 'md',
+        }}
+        variant='soft'
+      >
+        <Typography
+          level='body-md'
+          sx={{
+            display: 'inline-block',
+            whiteSpace: 'nowrap',
+            overflow: 'scroll',
+            textOverflow: 'ellipsis',
+            wordBreak: 'break-all',
+            wordWrap: 'break-word',
+            maxWidth: '90%',
+          }}
+        >
+          {(output !== undefined && output.toString()) ||
+            'Output will appear here. You can scroll the text if it becomes too long.'}
+        </Typography>
+      </Sheet>
+    </Stack>
+  )
+}
