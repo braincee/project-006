1f35cc37279d81bda4fab406f99c03dd5af8931a
braincee <kwesi938@gmail.com>
2023-11-16 18:10:50 +0000

    Add Hext to Number component

---
+++ components/web3js/utils/HexToNumber.tsx
@@ -0,0 +1,114 @@
+'use client'
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Sheet,
+  Stack,
+  Typography,
+} from '@mui/joy'
+import { useEffect, useState } from 'react'
+import { hexToNumber } from 'web3-utils'
+
+export default function HexToNumber() {
+
+    const [hexValue, setHexValue] = useState<string>('');
+    const [numberValue, setNumberValue] = useState<number | bigint>();
+  
+    const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+      const value = event.target.value;
+      setHexValue(value);
+    };
+  
+    useEffect(() => {
+      if (!hexValue) {
+        setNumberValue(undefined);
+        return;
+      }
+  
+      try {
+        const convertedNumber = hexToNumber(hexValue);
+        setNumberValue(convertedNumber);
+      } catch (error) {
+        console.error('Error converting hex to number:', error);
+        setNumberValue(undefined);
+      }
+    }, [hexValue]);
+
+  return (
+    <Stack
+      direction={{
+        sm: 'column',
+        md: 'row',
+      }}
+      spacing={2}
+      height={'100%'}
+      alignItems={'center'}
+    >
+      <Sheet
+        sx={{
+          height: '100%',
+          width: '100%',
+          maxWidth: {
+            md: '50%',
+            sm: '100%',
+          },
+          alignSelf: 'center',
+        }}
+      >
+        <FormControl
+          size='lg'
+          required={true}
+          sx={{
+            flexGrow: 1,
+          }}
+        >
+          <FormLabel>hex</FormLabel>
+          <Input
+            name='hexToString'
+            placeholder={'Native web3js "hex" parameter.'}
+            onChange={handleChange}
+            type='text'
+          />
+        </FormControl>
+      </Sheet>
+
+      <Sheet
+        sx={{
+          height: '100%',
+          minHeight: '100px',
+          width: '100%',
+          maxWidth: {
+            md: '50%',
+            sm: '100%',
+          },
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          alignSelf: 'center',
+          borderRadius: 'md',
+        }}
+        variant='soft'
+      >
+        <Typography
+          level='body-md'
+          sx={{
+            display: 'inline-block',
+            whiteSpace: 'nowrap',
+            overflow: 'scroll',
+            textOverflow: 'ellipsis',
+            wordBreak: 'break-all',
+            wordWrap: 'break-word',
+            maxWidth: '90%',
+          }}
+        >
+          {numberValue !== undefined
+            ? numberValue.toString()
+            : 
+            'Output will appear here. You can scroll the text if it becomes too long.'}
+        </Typography>
+      </Sheet>
+    </Stack>
+  )
+}
