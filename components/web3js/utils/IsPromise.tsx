3d0be09c3064ce306edf88b774047d804f5f3872
Achilles-Dev <solohag92@gmail.com>
2023-11-18 04:08:19 +0000

    Add to web3js utils components

---
+++ components/web3js/utils/IsPromise.tsx
@@ -0,0 +1,105 @@
+'use client'
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Sheet,
+  Stack,
+  Typography,
+} from '@mui/joy'
+import { useEffect, useState } from 'react'
+import { isPromise } from 'web3-utils'
+
+export default function IsPromise() {
+  const [data, setData] = useState<unknown>()
+
+  const [output, setOutput] = useState<boolean>()
+
+  const handleChange = (event: React.BaseSyntheticEvent) => {
+    const value = event.target.value
+
+    if (!value || value === '') {
+      setOutput(false)
+      return
+    }
+    if (typeof value != 'object' || typeof value != 'function') return
+    setData(value)
+  }
+
+  useEffect(() => {
+    if (!data || data === '') {
+      setOutput(false)
+      return
+    }
+    setOutput(isPromise(data))
+  }, [data])
+
+  return (
+    <Stack
+      direction={{
+        sm: 'column',
+        md: 'row',
+      }}
+      spacing={2}
+      height={'100%'}
+      alignItems={'center'}
+    >
+      <Sheet
+        sx={{
+          height: '100%',
+          width: '100%',
+          maxWidth: {
+            md: '50%',
+            sm: '100%',
+          },
+          alignSelf: 'center',
+        }}
+      >
+        <FormControl size='lg' required={true}>
+          <FormLabel>Object | Function</FormLabel>
+          <Input
+            name='object'
+            placeholder={`object || function with then function`}
+            onChange={handleChange}
+            type='string'
+          />
+        </FormControl>
+      </Sheet>
+
+      <Sheet
+        sx={{
+          height: '100%',
+          minHeight: '100px',
+          width: '100%',
+          maxWidth: {
+            md: '50%',
+            sm: '100%',
+          },
+          display: 'flex',
+          flexDirection: 'column',
+          justifyContent: 'center',
+          alignItems: 'center',
+          alignSelf: 'center',
+          borderRadius: 'md',
+        }}
+        variant='soft'
+      >
+        <Typography
+          level='body-md'
+          sx={{
+            display: 'inline-block',
+            whiteSpace: 'nowrap',
+            overflow: 'scroll',
+            textOverflow: 'ellipsis',
+            wordBreak: 'break-all',
+            wordWrap: 'break-word',
+            maxWidth: '90%',
+          }}
+        >
+          {output ||
+            'Output will appear here. You can scroll the text if it becomes too long.'}
+        </Typography>
+      </Sheet>
+    </Stack>
+  )
+}
