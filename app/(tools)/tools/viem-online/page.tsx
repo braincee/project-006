f5f76207feb4f9bc4fa6636b0919dbe7ad1fc417
Achilles-Dev <solohag92@gmail.com>
2023-11-21 09:55:41 +0000

    Update viem utils Addresses

--- app/(tools)/tools/viem-online/page.tsx
+++ app/(tools)/tools/viem-online/page.tsx
@@ -10,24 +10,21 @@ import ParseEther from '@/components/viem/utils/Units/ParseEther'
 import ParseGwei from '@/components/viem/utils/Units/ParseGwei'
 import ParseUnits from '@/components/viem/utils/Units/ParseUnits'
 
-
-
-
 import {
-    Accordion,
-    AccordionDetails,
-    AccordionSummary,
-    Button,
-    Divider,
-    FormControl,
-    FormHelperText,
-    FormLabel,
-    Input,
-    Sheet,
-    Stack,
-    Textarea,
-    ToggleButtonGroup,
-    Typography,
+  Accordion,
+  AccordionDetails,
+  AccordionSummary,
+  Button,
+  Divider,
+  FormControl,
+  FormHelperText,
+  FormLabel,
+  Input,
+  Sheet,
+  Stack,
+  Textarea,
+  ToggleButtonGroup,
+  Typography,
 } from '@mui/joy'
 import Concat from '@/components/viem/utils/Data/Concat'
 import IsBytes from '@/components/viem/utils/Data/IsBytes'
@@ -50,204 +47,199 @@ import NameHash from '@/components/viem/ens/NameHash'
 import Normalize from '@/components/viem/ens/Normalize'
 
 export default async function ViemOnline() {
-    return (
-        <Stack
-            direction={{ xs: 'column', md: 'column' }}
-            sx={{
-                height: '100%',
-                width: '100%',
-            }}
-            spacing={2}
-        >
-            <Typography level="h1">Viem Online</Typography>
-            <Typography level="h2">
-                All available viem functions will be placed below, each inside a FunctionContainer.
-            </Typography>
+  return (
+    <Stack
+      direction={{ xs: 'column', md: 'column' }}
+      sx={{
+        height: '100%',
+        width: '100%',
+      }}
+      spacing={2}
+    >
+      <Typography level='h1'>Viem Online</Typography>
+      <Typography level='h2'>
+        All available viem functions will be placed below, each inside a
+        FunctionContainer.
+      </Typography>
 
-            <Stack
-                direction={{ xs: 'column', md: 'column' }}
-                sx={{
-                    height: '100%',
-                    width: '100%',
-                }}
-                overflow={'scroll'}
-                spacing={2}
-            >
-                   <FunctionContainer
-                    title="viem.normalize"
-                    description="Enter ENS name."
-                >
-                 <Normalize />
-                  </FunctionContainer>
-                  <FunctionContainer
-                    title="viem.NameHash"
-                    description="Hashes ENS name."
-                >
-                 <NameHash />
-                  </FunctionContainer>
-                <FunctionContainer
-                    title="viem.labelHash"
-                    description="Hashes ENS label."
-                >
-                 <LabelHash />
-                  </FunctionContainer>
-                    <FunctionContainer
-                    title="viem.getEnsText"
-                    description="Gets a text record for specified ENS name."
-                >
-                 <GetEnsText />
-                  </FunctionContainer>
-                  <FunctionContainer
-                    title="viem.getEnsResolver"
-                    description="Gets resolver for ENS name."
-                >
-                 <GetEnsReslover />
-                  </FunctionContainer>
-                  <FunctionContainer
-                    title="viem.getEns"
-                    description="Gets primary name for specified address."
-                >
-                 <GetEnsName />
-                  </FunctionContainer>
-                  <FunctionContainer
-                    title="viem.getEnsAvatar"
-                    description="Gets the avatar of an ENS name."
-                >
-                 <GetEnsAvatar />
-                  </FunctionContainer>
-                  <FunctionContainer
-                    title="viem.getEnsAddress"
-                    description="Gets address for ENS name."
-                >
-                 <GetEnsAddress />
-                  </FunctionContainer>
-                   <FunctionContainer
-                    title="viem.fromHex"
-                    description="Decodes a hex value to a string, number or byte array."
-                >
-                 <FromHex />
-                  </FunctionContainer>
-                   <FunctionContainer
-                    title="viem.toHex"
-                    description="Encodes a string, number, boolean or byte array to a hex value value."
-                >
-                 <ToHex />
-                  </FunctionContainer>
-                 <FunctionContainer
-                    title="viem.toBytes"
-                    description="Encodes a string, hex value, number or boolean to a byte array."
-                >
-                 <ToBytes />
-                  </FunctionContainer>
-                  <FunctionContainer
-                    title="viem.fromRlp"
-                    description="Decodes a Recursive-Length Prefix (RLP) value into a decoded hex value or byte array."
-                >
-                 <FromRlp />
-                  </FunctionContainer>
-                 <FunctionContainer
-                    title="viem.pad"
-                    description="Pads a hex value or byte array with leading or trailing zeros."
-                >
-                 <Pad />
-                  </FunctionContainer>
-                <FunctionContainer
-                    title="viem.slice"
-                    description="Returns a section of the hex or byte array given a start/end bytes offset."
-                >
-                 <Slice />
-                  </FunctionContainer>
-                <FunctionContainer
-                    title="viem.trim"
-                    description="Trims the leading or trailing zero byte data from a hex value or byte array."
-                >
-                 <Trim />
-                  </FunctionContainer>
-                 <FunctionContainer
-                    title="viem.size"
-                    description="Retrieves the size of the value (in bytes)."
-                >
-                 <Size />
-                  </FunctionContainer>
-                 <FunctionContainer
-                    title="viem.isHex"
-                    description="Checks whether the value is a hex value or not."
-                >
-                  <IsHex />
-                  </FunctionContainer>
-                 <FunctionContainer
-                    title="viem.isBytes"
-                    description="Checks whether the value is a byte array or not."
-                >
-                  <IsBytes />
-                  </FunctionContainer>
-                <FunctionContainer
-                    title="viem.concat"
-                    description="Concatenates a set of hex values or byte arrays."
-                >
-                  <Concat />
-                  </FunctionContainer>
-                   <FunctionContainer
-                    title="viem.parseUnits"
-                    description="Multiplies a string representation of a number by a given exponent of base 10 (10exponent)."
-                >
-                  <ParseUnits />
-                  </FunctionContainer>
-                   <FunctionContainer
-                    title="viem.parseGwei"
-                    description="Converts a string representation of gwei to numerical wei."
-                >
-                  <ParseGwei />
-                  </FunctionContainer>
-                  <FunctionContainer
-                    title="viem.parseEther"
-                    description="Converts a string representation of ether to numerical wei."
-                >
-                  <ParseEther />
-                </FunctionContainer>
-                   <FunctionContainer
-                    title="viem.formatUnits"
-                    description="Divides a number by a given exponent of base 10 (10exponent), and formats it into a string representation of the number."
-                >
-                  <FormatUnits />
-                </FunctionContainer>
-                  <FunctionContainer
-                    title="viem.formatGwei"
-                    description="Converts numerical wei to a string representation of gwei."
-                >
-                  <FormatGwei />
-                </FunctionContainer>
-                  <FunctionContainer
-                    title="viem.formatEther"
-                    description="Converts numerical wei to a string representation of ether."
-                >
-                  <FormatEhter />
-                </FunctionContainer>
-                <FunctionContainer
-                    title="viem.isAddressEqual"
-                    description="Checks if the given addresses (checksummed) are equal."
-                >
-                    <IsAddressEqual />
-                </FunctionContainer>
-                <FunctionContainer
-                    title="viem.isAddressEqual"
-                    description="Checks the given address(checksummed)"
-                >
-                    <GetAddress />
-                </FunctionContainer>
-                <FunctionContainer
-                    title="viem.getContractAddress"
-                    description="Checks to get the contract address"
-                >
-                   <GetContractAddress />
-                </FunctionContainer>
-                <FunctionContainer
-                    title="viem.isAddressEqual"
-                    description="Checks for validation of address"
-                >
-                    <IsAddress />
-                </FunctionContainer>
-            </Stack>
-        </Stack>
-    )
+      <Stack
+        direction={{ xs: 'column', md: 'column' }}
+        sx={{
+          height: '100%',
+          width: '100%',
+        }}
+        overflow={'scroll'}
+        spacing={2}
+      >
+        <FunctionContainer title='viem.normalize' description='Enter ENS name.'>
+          <Normalize />
+        </FunctionContainer>
+        <FunctionContainer title='viem.nameHash' description='Hashes ENS name.'>
+          <NameHash />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.labelHash'
+          description='Hashes ENS label.'
+        >
+          <LabelHash />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.getEnsText'
+          description='Gets a text record for specified ENS name.'
+        >
+          <GetEnsText />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.getEnsResolver'
+          description='Gets resolver for ENS name.'
+        >
+          <GetEnsReslover />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.getEns'
+          description='Gets primary name for specified address.'
+        >
+          <GetEnsName />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.getEnsAvatar'
+          description='Gets the avatar of an ENS name.'
+        >
+          <GetEnsAvatar />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.getEnsAddress'
+          description='Gets address for ENS name.'
+        >
+          <GetEnsAddress />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.fromHex'
+          description='Decodes a hex value to a string, number or byte array.'
+        >
+          <FromHex />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.toHex'
+          description='Encodes a string, number, boolean or byte array to a hex value value.'
+        >
+          <ToHex />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.toBytes'
+          description='Encodes a string, hex value, number or boolean to a byte array.'
+        >
+          <ToBytes />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.fromRlp'
+          description='Decodes a Recursive-Length Prefix (RLP) value into a decoded hex value or byte array.'
+        >
+          <FromRlp />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.pad'
+          description='Pads a hex value or byte array with leading or trailing zeros.'
+        >
+          <Pad />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.slice'
+          description='Returns a section of the hex or byte array given a start/end bytes offset.'
+        >
+          <Slice />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.trim'
+          description='Trims the leading or trailing zero byte data from a hex value or byte array.'
+        >
+          <Trim />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.size'
+          description='Retrieves the size of the value (in bytes).'
+        >
+          <Size />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.isHex'
+          description='Checks whether the value is a hex value or not.'
+        >
+          <IsHex />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.isBytes'
+          description='Checks whether the value is a byte array or not.'
+        >
+          <IsBytes />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.concat'
+          description='Concatenates a set of hex values or byte arrays.'
+        >
+          <Concat />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.parseUnits'
+          description='Multiplies a string representation of a number by a given exponent of base 10 (10exponent).'
+        >
+          <ParseUnits />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.parseGwei'
+          description='Converts a string representation of gwei to numerical wei.'
+        >
+          <ParseGwei />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.parseEther'
+          description='Converts a string representation of ether to numerical wei.'
+        >
+          <ParseEther />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.formatUnits'
+          description='Divides a number by a given exponent of base 10 (10exponent), and formats it into a string representation of the number.'
+        >
+          <FormatUnits />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.formatGwei'
+          description='Converts numerical wei to a string representation of gwei.'
+        >
+          <FormatGwei />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.formatEther'
+          description='Converts numerical wei to a string representation of ether.'
+        >
+          <FormatEhter />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.isAddressEqual'
+          description='Checks if the given addresses (checksummed) are equal.'
+        >
+          <IsAddressEqual />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.isAddressEqual'
+          description='Checks the given address(checksummed)'
+        >
+          <GetAddress />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.getContractAddress'
+          description='Checks to get the contract address'
+        >
+          <GetContractAddress />
+        </FunctionContainer>
+        <FunctionContainer
+          title='viem.isAddressEqual'
+          description='Checks for validation of address'
+        >
+          <IsAddress />
+        </FunctionContainer>
+      </Stack>
+    </Stack>
+  )
 }
--- components/viem/utils/Addresses/GetContractAddress.tsx
+++ components/viem/utils/Addresses/GetContractAddress.tsx
@@ -1,35 +1,83 @@
 'use client'
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react'
 import {
   FormControl,
+  FormHelperText,
   FormLabel,
   Input,
+  Option,
+  Select,
   Sheet,
   Stack,
   Typography,
-} from '@mui/joy';
-import { getContractAddress } from 'viem';
+} from '@mui/joy'
+import {
+  ByteArray,
+  GetContractAddressOptions,
+  Hex,
+  getContractAddress,
+} from 'viem'
 
 export default function GetContractAddress() {
-  const [str, setStr] = useState('');
-  const [output, setOutput] = useState('');
+  const [from, setFrom] = useState('')
+  const [nonce, setNonce] = useState<bigint>()
+  const [opCode, setOpCode] = useState('')
+  const [byteCode, setByteCode] = useState('')
+  const [byteCodeHash, setByteCodeHash] = useState('')
+  const [salt, setSalt] = useState('')
+  const [output, setOutput] = useState('')
+
+  const handleFromChange = (event: React.BaseSyntheticEvent) => {
+    const value = event.target.value
+
+    if (!value || value === '') return
+    if (!value.startsWith('0x') || value.length !== 42) return
+    setFrom(value)
+  }
+
+  const handleSelect = (
+    event: React.SyntheticEvent | null,
+    newValue: string | null
+  ) => {
+    if (!newValue) return
+    setOpCode(newValue)
+  }
 
   useEffect(() => {
-    if (!str || str === '') return;
-
-    const fetchData = async () => {
-      try {
-        const contractAddress = await getContractAddress(str);
-        setOutput(contractAddress);
-      } catch (error) {
-        console.error('Error fetching contract address:', error);
-        setOutput('Error occurred. Please check the console for details.');
-      }
-    };
-
-    fetchData();
-  }, [str]);
+    let newFrom = from as `0x${string}`
+    let newByteCode = byteCode as ByteArray | Hex
+    let newByteCodeHash = byteCode as ByteArray | Hex
+    let newSalt = salt as Hex | ByteArray
+    if (opCode !== '' && nonce) {
+      setOutput(
+        getContractAddress({
+          from: newFrom,
+          nonce,
+        })
+      )
+      return
+    }
+    if (opCode && opCode === 'CREATE') {
+      let newOpcode = opCode as 'CREATE'
+      const opts = {
+        from: newFrom,
+        opcode: newOpcode,
+      } as GetContractAddressOptions
+      setOutput(getContractAddress(opts))
+      return
+    }
+    let newOpcode = opCode as 'CREATE2'
+    setOutput(
+      getContractAddress({
+        from: newFrom,
+        opcode: newOpcode,
+        bytecode: newByteCode,
+        bytecodeHash: newByteCodeHash,
+        salt: newSalt,
+      })
+    )
+  }, [from, nonce, opCode, byteCode, salt])
 
   return (
     <Stack
@@ -52,13 +100,59 @@ export default function GetContractAddress() {
           alignSelf: 'center',
         }}
       >
-        <FormControl size="lg" required={true} sx={{ flexGrow: 1 }}>
-          <FormLabel>str</FormLabel>
+        <FormControl size='lg' sx={{ flexGrow: 1 }}>
+          <FormLabel>From</FormLabel>
           <Input
-            name="getContractAddress"
+            name='from'
             placeholder={'Native viem "str" parameter.'}
-            onChange={(e) => setStr(e.target.value)}
-            type="text"
+            onChange={(e) => setFrom(e.target.value)}
+            type='text'
+          />
+        </FormControl>
+        <FormControl size='lg' sx={{ flexGrow: 1 }}>
+          <FormLabel>Nonce</FormLabel>
+          <Input
+            name='nonce'
+            placeholder={'Native viem "bigInt" parameter.'}
+            onChange={(e) => setNonce(BigInt(e.target.value))}
+            type='number'
+          />
+        </FormControl>
+        <FormControl size='lg'>
+          <FormLabel>Opcode</FormLabel>
+          <Select placeholder={'opcode'} onChange={handleSelect}>
+            <Option value={'CREATE'}>CREATE</Option>
+            <Option value={'CREATE2'}>CREATE2</Option>
+          </Select>
+          <FormHelperText>
+            <Typography level='body-xs'>Opcode</Typography>
+          </FormHelperText>
+        </FormControl>
+        <FormControl size='lg' sx={{ flexGrow: 1 }}>
+          <FormLabel>Byte Code</FormLabel>
+          <Input
+            name='byteCode'
+            placeholder={'Native viem "str" parameter.'}
+            onChange={(e) => setByteCode(e.target.value)}
+            type='text'
+          />
+        </FormControl>
+        <FormControl size='lg' sx={{ flexGrow: 1 }}>
+          <FormLabel>Byte Code Hash</FormLabel>
+          <Input
+            name='byteCodeHash'
+            placeholder={'Native viem "str" parameter.'}
+            onChange={(e) => setByteCodeHash(e.target.value)}
+            type='text'
+          />
+        </FormControl>
+        <FormControl size='lg' sx={{ flexGrow: 1 }}>
+          <FormLabel>Salt</FormLabel>
+          <Input
+            name='salt'
+            placeholder={'Native viem "str" parameter.'}
+            onChange={(e) => setSalt(e.target.value)}
+            type='text'
           />
         </FormControl>
       </Sheet>
@@ -79,10 +173,10 @@ export default function GetContractAddress() {
           alignSelf: 'center',
           borderRadius: 'md',
         }}
-        variant="soft"
+        variant='soft'
       >
         <Typography
-          level="body-md"
+          level='body-md'
           sx={{
             display: 'inline-block',
             whiteSpace: 'nowrap',
@@ -98,5 +192,5 @@ export default function GetContractAddress() {
         </Typography>
       </Sheet>
     </Stack>
-  );
+  )
 }
